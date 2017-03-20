78d550de0688cc3a12d2ae8f0d47e3cc901880c7
yozu <yasuo@ozu.email>
2017-03-20 19:41:03 +0900

    implemented app fetchGakubu *

--- app.js
+++ app.js
@@ -11,13 +11,36 @@ if (process.argv[2] == "searchGakubu" && process.argv[3]) {
 		console.log(item);
 	}
 } else if (process.argv[2] == "fetchGakubu" && process.argv[3]) {
-	if (!gakubu.getPageIDList(process.argv[3], function(data){
-		for (let record of data) console.log(record);
-	})) {
-		process.stderr.write("error: gakubu not found\n");
+	let gidList = [];
+	let pidList = [];
+	if (process.argv[3] == "*") {
+		let list = gakubu.search(null);
+		for (let item of list) {
+			gidList.push(item.id);
+		}
+	} else {
+		gidList = [process.argv[3]];
 	}
+	function loop() {
+		let gid = gidList.shift();
+		process.stderr.write("fetching gakubu " + gid + "...\n");
+		if (!gakubu.getPageIDList(gid, function(data){
+			for (let record of data) pidList.push(record);
+			if (gidList.length) {
+				loop();
+			} else {
+				for (let record of pidList) {
+					process.stdout.write(record + "\n");
+				}
+			}
+		})) {
+			process.stderr.write("error: " + gid + " not found\n");
+		}
+	}
+	loop();
 } else {
 	console.log("node app searchGakubu <keyword>");
 	console.log("node app listGakubu");
 	console.log("node app fetchGakubu <gid>");
+	console.log("node app fetchGakubu '*'");
 }
